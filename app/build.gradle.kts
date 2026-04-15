3f269dd3537cacd2ee722fe3ef1596e10bce398b
Nemo <140950079+iejdudbd7@users.noreply.github.com>
2026-04-15 18:52:31 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,9 +18,9 @@ android {
 
     signingConfigs {
         create("release") {
-            storeFile = file(System.getenv("KEYSTORE_FILE") ?: "../release.keystore")
+            storeFile = file("release.keystore")
             storePassword = System.getenv("KEYSTORE_PASSWORD")
-            keyAlias = System.getenv("KEY_ALIAS")
+            keyAlias = "release"
             keyPassword = System.getenv("KEY_PASSWORD")
         }
     }
