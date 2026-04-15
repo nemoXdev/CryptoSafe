94a332bc84e79e3da7fbb3d6653b62be9a921ffd
Nemo <140950079+iejdudbd7@users.noreply.github.com>
2026-04-15 13:46:38 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,19 @@
 pluginManagement {
     repositories {
-        google()
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
         mavenCentral()
         gradlePluginPortal()
+    }
+    plugins {
+        id("com.android.application") version "8.2.2" apply false
+        id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+        id("org.jetbrains.kotlin.plugin.compose") version "1.9.22" apply false
     }
 }
 
