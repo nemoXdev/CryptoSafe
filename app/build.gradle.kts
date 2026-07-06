80fd2d445a3afbc8b914cddada584d31af9efa6d
nemoXdev <nemoXdev@users.noreply.github.com>
2026-07-06 15:31:56 +0000

    Version update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.cryptosafe.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.2.1"
+        versionCode = 6
+        versionName = "1.2.2"
     }
 
     signingConfigs {
